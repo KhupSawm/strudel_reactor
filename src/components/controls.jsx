765fa993c4d8dbe69ae8021919d4ef67aa86691f
KhupSawm <trnlsun@gmail.com>
2025-11-16 15:32:30 +1030

    feat(ui): Removed proc&play and process button as not needed anymore.

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -3,8 +3,8 @@
 export default function Controls({ onProcess, onProcPlay, onPlay, onStop, replay, isReady}) {
     return (
         <div className="music-controls d-flex justify-content-center align-items-center gap-3 my-3">
-            <button className="control-btn" title="Process" onClick={onProcess}><FaCogs /></button>
-            <button className="control-btn" title="Proc&Play" onClick={onProcPlay}><FaPlayCircle /></button>
+            {/*<button className="control-btn" title="Process" onClick={onProcess}><FaCogs /></button>*/}
+            {/*<button className="control-btn" title="Proc&Play" onClick={onProcPlay}><FaPlayCircle /></button>*/}
             <button disabled={!isReady} className="control-btn stop-btn" title="Stop" onClick={onStop}><FaStop /></button>
             <button disabled={!isReady} className="control-btn play-btn" title="Play" onClick={onPlay}><FaPlay /></button>
             <button disabled={!isReady} className="control-btn redo-btn" title="Reload" onClick={replay}><FaRedo /></button>    
